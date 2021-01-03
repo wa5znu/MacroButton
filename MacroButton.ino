e4367eaa420747f756e389068dfa68d0478ded87
Leigh Klotz <klotz@klotz.me>
2021-01-02 18:39:54 -0800

    Initial Commit

---
+++ MacroButton.ino
@@ -0,0 +1,143 @@
+/*
+ * based on http://www.arduino.cc/en/Tutorial/KeyboardMessage
+ * https://digistump.com/wiki/digispark/tutorials/button
+ * Copyright 2020 Leigh Klotz WA5ZNU <leigh@wa5znu.org>
+ */
+
+#include "DigiKeyboard.h"
+#include "config.h"
+
+#define PRESSED (HIGH)
+#define RELEASED (LOW)
+
+// Rev4 marking: The on-board LED is connected to P1.
+// sadly I connected both to pin 1.  The multiplex works though.
+const int PIN=1;
+
+const long TIMEOUT = 1500;
+const long DEBOUNCE_DELAY = 25;
+const long DIT_DAH_THRESHOLD = 200; // todo: should be auto-threshold
+const long ELEMENT_END_DELAY = 400;
+
+int button_state = RELEASED;
+int last_button_state = RELEASED;
+long last_pressed_time = 0;
+long last_released_time = 0;
+long last_debounce_time = 0;
+int state_index = 0;
+
+const int MAX_RX_LEN = MAX_KEY_LEN+1;
+const char KEYS[N_KEYS][MAX_KEY_LEN] = MESSAGE_KEYS;
+const char* MESSAGES[N_KEYS] = MESSAGE_VALUES;
+char RX[MAX_RX_LEN];		// +1 for null terminator
+
+void clearState();
+void handleButton(int button_state, long delta);
+void send();
+void blink(int d);
+
+void setup() {
+  pinMode(PIN, OUTPUT);
+  digitalWrite(PIN, HIGH);
+  DigiKeyboard.delay(1);
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(250);
+  digitalWrite(PIN, LOW);
+  clearState();
+  blink(100);
+}
+
+void loop() {
+  pinMode(PIN, INPUT);
+  int reading = digitalRead(PIN);
+
+  long now = millis();
+  if (reading != last_button_state) {
+    last_debounce_time = now;
+  }
+
+  if ((now - last_debounce_time) > DEBOUNCE_DELAY) {
+    if (reading != button_state) {
+      button_state = reading;
+      if (button_state == PRESSED) {
+	last_pressed_time = now;
+      }	else if (button_state == RELEASED) {
+	long delta_keydown = now - last_pressed_time;
+	last_pressed_time = 0;
+	handleButton(button_state, delta_keydown);
+	last_released_time = now;
+      } else {
+	DigiKeyboard.println(F("BAD BUTTON STATE"));
+      }
+    }
+  }
+
+  if (state_index > 0 && button_state == RELEASED) {
+    long delta = (now - last_released_time);
+    if (delta > TIMEOUT) {
+      handleTimeout();
+    } else if (delta > ELEMENT_END_DELAY) {
+      int k = key_matching_rx();
+      if (k >= 0) {
+	send(k);
+	clearState();
+      }
+    } 
+  } 
+
+  last_button_state = reading;
+}
+
+void handleButton(int button_state, long delta) {
+  if (state_index+1 < MAX_RX_LEN) {
+    char elt = ((delta <= DIT_DAH_THRESHOLD) ? '*' : '-');
+    RX[state_index++] = elt;
+    RX[state_index] = '\0';
+  } else {
+    blink(250);
+  }
+}
+
+void clearState() {
+  state_index = 0;
+  last_debounce_time = 0;
+  last_pressed_time = 0;
+  last_released_time = 0;
+  last_button_state = RELEASED;
+  for (int i = 0; i < MAX_RX_LEN; i++) {
+    RX[i] = '\0';
+  }
+}
+
+void handleTimeout() {
+  clearState();
+  blink(50);
+}
+
+int key_matching_rx() {
+  if (state_index > 0) {
+    for (int k = 0; k < N_KEYS; k++) {
+      if (strcmp(RX, KEYS[k]) == 0) {
+	return k;
+      }
+    }
+  }
+  return -1;
+}
+
+void blink(int d) {
+  for (int i = 0; i < 5; i++) {
+    pinMode(PIN, OUTPUT);
+    digitalWrite(PIN, HIGH);
+    DigiKeyboard.delay(d);
+    digitalWrite(PIN, LOW);
+    DigiKeyboard.delay(d);
+  }
+}
+
+void send(int k) {
+  pinMode(PIN, OUTPUT);
+  digitalWrite(PIN, HIGH);
+  DigiKeyboard.println(MESSAGES[k]);
+  digitalWrite(PIN, LOW);
+}
---
+++ README.md
@@ -0,0 +1,33 @@
+# Macro Button
+
+This project uses the DigiSpark USB Development Board v4, the DigiStump Button board, and a few soldered headers to make a keyboard macro input device.
+
+The device is capable of sending one or more pre-programmed strings in response to a single character of Morse Code input.  This input method allows for a single-key macro keyboard, with a modicum of security by obscurity through the use of a Morse code letter.
+
+# Hardware
+DigiStump's DigiSpark was the result of a Kickstarter project, and has undergone a few revisions, and the company has branched into other lines of hardware. This project uses an early DigiSpark, which is a small Atmel ATTINY85 supported by the library written by the kind folks at DigiSpark.com.
+
+![Image of Front](docs/front.jpg) ![Image of Back](docs/back.jpg)
+
+# Issues I encountered
+I encountered the following issues developing this sketch:
+
+## Hardware Issues
+The DigiSpark board I have is marked V4, and it has the LED on Pin 0. Unfortunately, the original DigiSpark (no-rev marker, and some other numbers as well) have the LED on pin 1. I mistook the board configuration when soldering the button in place and tied it to Pin 0, the LED pin. That means that the LED is on if the button is pressed, but software controllable at other times. In order to multiplex and save my hardware config, I multiplexed the pin by using pinMode before each LED and each button operation.
+
+The DigiKeyboard.delay function appears to run twice as quickly as the spec, so perhaps I need to set the OSC CAL register.
+
+## Readiness issue
+The DigiSpark boot loader has a 5-second timeout before it starts running application code. It's clear that this is the case because you can program it only in the first 5 seconds after being plugged in, but it's still sometimes confusing, especially if you're trying to debug timing issues. So, I added a moderate LED ON time at the start of setup(), followed by a rapid set of blinks at the end of setup. This makes it easier to judge whether the device is up and running. 
+
+## Debouncing Issues
+Device general reliability was low, and after a number of other causes (boot time, hardware pin conflicts, timing issues, etc.) were resolved. At this point I had good LED and "print" statement feedback, and found that the button was hard to trigger and very bouncy when pressed hard enough to trigger. I cleaned it with electronics cleaner and found it much more reliable, though still bouncy.
+
+# References
+- DigiStump Hardware http://digistump.com/
+- DigiSpark USB Development Board http://digistump.com/products/1
+- DigiSpark USB Development Board Arduino IDE "Board" link http://digistump.com/package_digistump_index.json
+- Arduino Keyboard library Tutorial example http://www.arduino.cc/en/Tutorial/KeyboardMessage
+- DigiSpark Button Tutorial example https://digistump.com/wiki/digispark/tutorials/button
+- Debouncing Arduino tutorial https://www.arduino.cc/en/Tutorial/BuiltInExamples/Debounce
+- Debouncing from Adafruit https://learn.adafruit.com/make-it-switch/debouncing
